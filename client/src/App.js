23b8a6cfa91eb8c565d341cb73a7ee68368ffd93
Sid Björk <sidricobjork@gmail.com>
2024-07-14 15:01:33 +0200

    add

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,28 @@ function App() {
       <div className=" pb-4">
         <h2 className="font-semibold">AI News</h2>
         <p>Simple news site where administrators can upload articles, where users can read, like and comment.</p>
-        <p>Link to live website: <a className="hover:underline" href="https://blog-app-client-blue.vercel.app/">https://blog-app-client-blue.vercel.app/</a></p>
-        <p>Link to github repository (client): <a className="hover:underline" href="https://github.com/SuphaNinja/BLOG_APP_CLIENT">https://github.com/SuphaNinja/BLOG_APP_CLIENT</a></p>
-        <p>Link to github repository (server): <a className="hover:underline" href="https://github.com/SuphaNinja/BLOG_APP_SERVER">https://github.com/SuphaNinja/BLOG_APP_SERVER</a></p>
+        <p>Link to live website: <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://blog-app-client-blue.vercel.app/">https://blog-app-client-blue.vercel.app/</a></p>
+        <p>Link to github repository (client): <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://github.com/SuphaNinja/BLOG_APP_CLIENT">https://github.com/SuphaNinja/BLOG_APP_CLIENT</a></p>
+        <p>Link to github repository (server): <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://github.com/SuphaNinja/BLOG_APP_SERVER">https://github.com/SuphaNinja/BLOG_APP_SERVER</a></p>
       </div>
       <div className=" pb-4">
         <h2 className="font-semibold">League Memory Game</h2>
         <p>Websocket based card memory game where you can add friends and play 1 vs 1 against them.</p>
-        <p>Link to live website: <a className="hover:underline" href="https://github.com/SuphaNinja/LOL_MEMORY_GAME">https://lol-memory-game-ten.vercel.app/</a></p>
-        <p>Link to github repository: <a className="hover:underline" href="https://github.com/SuphaNinja/LOL_MEMORY_GAME">https://github.com/SuphaNinja/LOL_MEMORY_GAME</a></p>
+        <p>Link to live website: <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://github.com/SuphaNinja/LOL_MEMORY_GAME">https://lol-memory-game-ten.vercel.app/</a></p>
+        <p>Link to github repository: <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://github.com/SuphaNinja/LOL_MEMORY_GAME">https://github.com/SuphaNinja/LOL_MEMORY_GAME</a></p>
       </div>
       <div className=" pb-4">
         <h2 className="font-semibold">GoalFitPro</h2>
         <p>AI powered gym guide generator where you fill out a form and get a 7 day personalized fitness plan.</p>
-        <p>Link to live website: <a className="hover:underline" href="https://aigymapp-client-wvix.vercel.app/">https://aigymapp-client-wvix.vercel.app/</a></p>
-        <p>Link to github repository (client): <a className="hover:underline" href="https://github.com/SuphaNinja/AIGYMAPP-CLIENT">https://github.com/SuphaNinja/AIGYMAPP-CLIENT</a></p>
-        <p>Link to github repository (server): <a className="hover:underline" href="https://github.com/SuphaNinja/AIGYMAPP-SERVER">https://github.com/SuphaNinja/AIGYMAPP-SERVER</a></p>
+        <p>Link to live website: <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://aigymapp-client-wvix.vercel.app/">https://aigymapp-client-wvix.vercel.app/</a></p>
+        <p>Link to github repository (client): <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://github.com/SuphaNinja/AIGYMAPP-CLIENT">https://github.com/SuphaNinja/AIGYMAPP-CLIENT</a></p>
+        <p>Link to github repository (server): <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://github.com/SuphaNinja/AIGYMAPP-SERVER">https://github.com/SuphaNinja/AIGYMAPP-SERVER</a></p>
       </div>
       <div className=" pb-4">
         <h2 className="font-semibold">ElectroBuy</h2>
         <p>Ecommerce store for browsing, adding to cart and checking out various products.</p>
-        <p>Link to live website: <a className="hover:underline" href="https://ecommerce-app-three-zeta.vercel.app/">https://ecommerce-app-three-zeta.vercel.app/</a></p>
-        <p>Link to github repository: <a className="hover:underline" href="https://github.com/SuphaNinja/ECOMMERCE_APP">https://github.com/SuphaNinja/ECOMMERCE_APP</a></p>
+        <p>Link to live website: <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://ecommerce-app-three-zeta.vercel.app/">https://ecommerce-app-three-zeta.vercel.app/</a></p>
+        <p>Link to github repository: <a className="text-blue-500 hover:text-purple-500 transition-colors hover:underline" href="https://github.com/SuphaNinja/ECOMMERCE_APP">https://github.com/SuphaNinja/ECOMMERCE_APP</a></p>
       </div>
     </div>
   );
